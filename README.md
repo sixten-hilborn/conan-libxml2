6b1c4c3f582cfc93549bfa592a9e8d194ba89a5b
Sixten Hilborn <hilborn90@gmail.com>
2017-05-26 10:26:10 +0200

    Update build.py

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 [![Build Status](https://travis-ci.org/sixten-hilborn/conan-libxml2.svg?branch=release/2.9.3)](https://travis-ci.org/sixten-hilborn/conan-libxml2)
-[![Build status](https://ci.appveyor.com/api/projects/status/n6177c207upj6j1p/branch/release/2.9.3?svg=true)](https://ci.appveyor.com/project/sixten-hilborn/conan-libxml2-75qg3/branch/release/2.9.3)
+[![Build status](https://ci.appveyor.com/api/projects/status/kagn5818ikgckgdo/branch/release/2.9.3?svg=true)](https://ci.appveyor.com/project/sixten-hilborn/conan-libxml2/branch/release/2.9.3)
 
 # conan-libxml2
 
--- build.py
+++ build.py
@@ -8,18 +8,18 @@ if __name__ == "__main__":
 
     if platform.system() == "Darwin":
         filtered_builds = []
-        for settings, options in builder.builds:
+        for settings, options, env_vars, build_requires in builder.builds:
             # Some isues with OSx and x86 with iconv, I think it could be linking with the system iconv and fails to link.
             if settings["arch"] != "x86":
-                filtered_builds.append([settings, options])
+                filtered_builds.append([settings, options, env_vars, build_requires])
         builder.builds = filtered_builds
 
     if platform.system() == "Windows":
         filtered_builds = []
-        for settings, options in builder.builds:
+        for settings, options, env_vars, build_requires in builder.builds:
             print settings
             if settings["compiler"] != "Visual Studio" or options["libxml2:shared"] == True:
-                filtered_builds.append([settings, options])
+                filtered_builds.append([settings, options, env_vars, build_requires])
         builder.builds = filtered_builds
 
     builder.run()
